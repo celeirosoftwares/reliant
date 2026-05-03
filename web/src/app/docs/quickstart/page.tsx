db6a6e4f93324379011285dea555bb7bafce2db7
Celeiro Softwares <celeirosoftwares@gmail.com>
2026-05-03 16:33:11 -0300

    fix: fix docs css import path

--- web/src/app/docs/quickstart/page.tsx
+++ web/src/app/docs/quickstart/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '../../docs.module.css'
+import styles from '@/components/dashboard/Docs.module.css'
 
 export default function QuickstartPage() {
   return (
